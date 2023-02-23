f7038861ae1d34df569ec0b47a5c8e89347f4a6d
Govindarajan V <govindarajanv@gmail.com>
2023-02-23 14:21:49 +0000

    fixed issues

--- hooks/JenkinsfileLint.sh
+++ hooks/JenkinsfileLint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 check=$(which docker)
+
 if [ $check != "" ]; then
     docker run --rm -u "$(id -u):$(id -g)" -w=/tmp -v "$PWD":/tmp nvuillam/npm-groovy-lint "$@" --failon error --verbose
     exit 0
