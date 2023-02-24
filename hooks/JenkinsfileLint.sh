7f94dd3a8a5dcb343309305dabb31d4e6c1fe46a
Govindarajan V <govindarajanv@gmail.com>
2023-02-24 09:41:12 +0530

    Added support for arm64

--- hooks/JenkinsfileLint.sh
+++ hooks/JenkinsfileLint.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 check=$(which docker)
-
 if [ $check != "" ]; then
-    docker run --rm -u "$(id -u):$(id -g)" -w=/tmp -v "$PWD":/tmp nvuillam/npm-groovy-lint "$@" --failon error --verbose
+    if [[ `uname -m` == "arm64" ]]; then
+        echo "arm64"
+        docker run --rm --platform linux/amd64 -u "$(id -u):$(id -g)" -w=/tmp -v "$PWD":/tmp nvuillam/npm-groovy-lint "$@" --failon error --verbose
+    else
+        echo "amd64"
+        docker run --rm -u "$(id -u):$(id -g)" -w=/tmp -v "$PWD":/tmp nvuillam/npm-groovy-lint "$@" --failon error --verbose
+    fi  
     exit 0
 else
   echo "Docker is not installed or running"
